28dbe23557dd40cf168a8126e40f12423b28a35d
EmilioCaldas888 <emiliocaldas888@gmail.com>
2020-12-05 19:12:19 -0300

    first class: position

--- src/applicaton/Program.java
+++ src/applicaton/Program.java
@@ -1,9 +1,12 @@
 package applicaton;
 
+import BoardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-	System.out.println("hello word");
+	Position pos = new Position(3,5);
+	System.out.println(pos);
 	}
 
 }
